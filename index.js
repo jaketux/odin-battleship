7bdeb27a46e9ea268ebc97678d7a61dd24605230
jaketux <169253660+jaketux@users.noreply.github.com>
2024-12-30 16:22:11 +1100

    Working on Battleship Project for The Odin Project
    I've now started working on the placeShip function that will place the ship based on the coordinates provided. This needs more work though.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function shipModule() {
 
-    function checkStatus(ship){
+    const checkStatus = function(ship){
         let lengthOfShip = ship.shipLength
         let countOfHits = 0
         for (i = 0; i<lengthOfShip;i++){
@@ -15,68 +15,92 @@ function shipModule() {
         }
     }
 
-    function hit(index,ship){
+    const hit = function(index,ship){
         ship.numberOfHits[index] = "X"
         ship.shipSunk = checkStatus(ship)
         return ship
     }
 
-    const carrierShip = {
+    const ships = [{
+        name: "carrierShip",
         numberOfHits: [,,,,,],
         shipLength: 5,
         shipSunk: checkStatus(this)
-    }
-
-    const battleShip = {
+    },
+    {
+        name: "battleShip",
         numberOfHits: [,,,,],
         shipLength: 4,
         shipSunk: checkStatus(this)
-    }
-
-    const destroyerShip = {
+    },
+    {
+        name: "destroyerShip",
         numberOfHits: [,,,],
         shipLength: 3,
         shipSunk: checkStatus(this)
-    }
-
-    const submarineShip = {
+    },
+    {
+        name: "submarineShip",
         numberOfHits: [,,,],
         shipLength: 3,
         shipSunk: checkStatus(this)
-    }
-
-    const patrolBoatShip = {
+    },
+    {
+        name: "patrolBoatShip",
         numberOfHits: [,,],
         shipLength: 2,
         shipSunk: checkStatus(this)
+    }]
+
+    const getShips = function(){
+        return ships
     }
 
-    return {checkStatus,hit}
+    return {getShips, checkStatus, hit}
 }
  
+const battleShipModule = shipModule()
 
-function gameBoard (){
+function gameBoardModule() {
+
+    const ships = battleShipModule.getShips()
+
+    // create a 10 by 10 2d grid
     let gameBoardArray = [
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-    [,,,,,,,,,,],
-]
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,]
+    ]
+
+    const getGameBoard = function(){
+        return gameBoardArray
+    }
+
+// allow each of the ships to be placed and occupy the grid in either horizontal or vertical orientation
+    const placeShip = function (ship, x, y, direction){
+
+        //horizontal direction
+        if (direction === "horizontal"){
+            for (i=0;i<ship.shipLength;i++){
+                gameBoardArray[x][y] = ship.name
+            }
+        }
+        // //vertical direction
+        // if (direction === "vertical"){
+
+        return gameBoardArray
+    }
+
+    return {placeShip, ships, getGameBoard}
 
 }
 
-// create a 10 by 10 grid
 
-// allow each of the ships to occupy the grid in either horizontal or vertical orientation
-
-const battleShipModule = shipModule()
-
-
-
-module.exports = shipModule
+module.exports = {shipModule: shipModule, gameBoardModule: gameBoardModule}
--- index.test.js
+++ index.test.js
@@ -1,6 +1,8 @@
-const shipModule = require('./index')
+const {shipModule: shipModule, gameBoardModule: gameBoardModule} = require('./index')
 
 const ship = shipModule()
+
+const gameBoard = gameBoardModule()
 
 // test to check a hit of the boat and whether it is sunk 
 
@@ -13,4 +15,19 @@ test('Check the number of hits for the boat', () =>  {
         shipLength: 5,
         shipSunk: true
     })
+})
+
+test('Check to see whether a boat is being placed horizontally correctly', () => {
+    expect (gameBoard.placeShip(gameBoard.ships[0],0,0,"horizontal")).toEqual([
+        ["carrierShip",,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,],
+        [,,,,,,,,,,]
+    ])
 })
